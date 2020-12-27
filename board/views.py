633f47720a0be2c9e5c00be06e4f421bfa36888b
menahassan <menah9@gmail.com>
2020-12-27 12:40:33 -0800

    finished starter code

--- board/views.py
+++ board/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+cards = ["covid exposure","mask/ppe","stay at home","superspreader",
+"quarantine", "stimulus check","event cancellation","covid forecast",
+"trump","fauci","NFL","pfizer","zoom"]
+
 # Create your views here.
 def index(request):
     return render(request, "board/index.html",{
-        "name": "Friend"
+        "cards": cards
     })
